44ca79618bfda01b84f4756a70058bae832fa65c
Garima Purohit <purohitgarima2307@gmail.com>
2026-06-15 01:03:22 +0530

    added the react components

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+function App() {
+  return (
+    <>
+    <Navbar />
+        <h1>hello guyzz</h1>
+    </>
+  );
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+    return (
+        <nav> 
+            <h1>TaskFlow</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>pages</li>
+                <li>about us</li>
+            </ul>
+            </nav>
+    );
+}
+export default Navbar;
